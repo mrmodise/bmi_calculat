347195a2bf941581510f7bb81435eedcb0793fc7
mrmodise <modisemorebodi@gmail.com>
2022-04-12 22:58:14 +0200

    feat: implement navigation

--- lib/menu_drawer.dart
+++ lib/menu_drawer.dart
@@ -1,3 +1,5 @@
+import 'package:bmi_calculator/bmi_screen.dart';
+import 'package:bmi_calculator/intro_screen.dart';
 import 'package:flutter/material.dart';
 
 class MenuDrawer extends StatelessWidget {
@@ -27,9 +29,23 @@ class MenuDrawer extends StatelessWidget {
         child: Text('Globo Fitness',
             style: TextStyle(color: Colors.white, fontSize: 28))));
     for (var element in menuTitles) {
+      Widget screen = Container();
       menuItems.add(ListTile(
         title: Text(element, style: const TextStyle(fontSize: 18)),
-        onTap: () {},
+        onTap: () {
+          switch (element) {
+            case 'Home':
+              screen = const IntroScreen();
+              break;
+            case 'BMI Calculator':
+              screen = const BmiScreen();
+              break;
+            default:
+          }
+          // Navigator.of(context).pop();
+          Navigator.of(context)
+              .push(MaterialPageRoute(builder: (context) => screen));
+        },
       ));
     }
     return menuItems;
